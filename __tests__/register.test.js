2a3a32588c4434375b670bee96ed0a107c7f9234
Neil Bourgeois <neil.bourgeois@trustscience.com>
2020-04-17 08:37:58 -0600

    adding tests for register endpoint

---
+++ __tests__/register.test.js
@@ -0,0 +1,30 @@
+const register = require('../register');
+
+const buildEvent = body => ({
+  body: JSON.stringify(body),
+});
+
+describe('Register endpoint', () => {
+  it('should return a bad request response when body is not present', async () => {
+    const event = buildEvent();
+    const response = await register.handler(event);
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return a bad request response when username or password is not present', async () => {
+    const event = buildEvent({});
+    const response = await register.handler(event);
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(400);
+    expect(JSON.parse(response.body).message).toBe('Username and password are required');
+  });
+
+  it('should return a success response', async () => {
+    const event = buildEvent({ username: 'moose@mailinator.com', password: 'I<3Squirrels' });
+    const response = await register.handler(event);
+    expect(response).toBeDefined();
+    expect(response.statusCode).toBe(200);
+    expect(JSON.parse(response.body).message).toBe('Successfully registered moose@mailinator.com');
+  });
+});
