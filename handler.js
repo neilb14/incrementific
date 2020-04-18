ff5c7ba88ac60db42ad0d0223b7ebb1792bd8d75
Neil Bourgeois <neil.bourgeois@trustscience.com>
2020-04-18 09:07:58 -0600

    add endpoints for counter

--- handler.js
+++ handler.js
@@ -1,16 +1,29 @@
 const storage = require('./lib/storage');
+const buildResponse = require('./lib/build-response');
 
-module.exports.hello = async event => {
+module.exports.next = async event => {
   const result = await storage.incrementKey('hello');
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        result,
-      },
-      null,
-      2
-    ),
-  };
+  return buildResponse.success({
+    message: 'Incremented',
+    result
+  });
 };
+
+module.exports.current = async event => {
+  const result = await storage.getKey('hello')
+  return buildResponse.success({
+    message: 'Current',
+    result,
+  });
+};
+
+module.exports.set = async event => {
+  if (!event.body || !/^current=\d+/.test(event.body)) return buildResponse.badRequest('expected current=value in payload');
+  const value = event.body.split('=')[1];
+  const result = await storage.setKey('hello', value);
+  return buildResponse.success({
+    message: 'Set',
+    result,
+  })
+};
+
