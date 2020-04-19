56777fb7a40e496fd551288233f64606e9be37cb
Neil Bourgeois <neil.bourgeois@gmail.com>
2020-04-19 08:52:26 -0600

    enable cors in backend

--- backend/src/lib/build-response.js
+++ backend/src/lib/build-response.js
@@ -1,5 +1,9 @@
 const buildResponse = (statusCode, body) => ({
     statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(
       body,
       null,
